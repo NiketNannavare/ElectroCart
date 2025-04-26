4ca312772c1a83689f7a3be2fea82048cc0875d8
NiketNannavare <niketnannaware703@gmail.com>
2025-04-26 15:57:21 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,28 +1,26 @@
 import mongoose from "mongoose";
 
-let cached=global.mongoose
+let cached = global.mongoose;
 
-if(!cached){
-    cached=global.mongoose={conn:null,promise:null}
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function ConnectDB() {
-
-    if(cached.conn){
-        return cached.conn
-    }
-    if(!cached.promise){
-        const opts={
-            bufferCommanda:false
-        }
-        cached.promise=mongoose.connect(`${process.env.MONGODB_URI}/electrocart`,opts).then(mongoose=>{
-            return mongoose
-        })
+    if (cached.conn) {
+        return cached.conn;
     }
 
-    cached.conn=await cached.promise
-    return cached.conn
-    
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "electrocart", 
+        }).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
 
-export default ConnectDB
+export default ConnectDB;
